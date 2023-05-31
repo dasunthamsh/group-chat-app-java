9f06e1286d79ff23630802a5e901a353f935a08a
dasunthamsh <tunathamash@gmail.com>
2023-05-31 23:30:04 +0530

    new modules added

---
+++ clientTow/src/controller/ClientTowFormController.java
@@ -0,0 +1,17 @@
+package controller;/*
+    @author Dasun
+*/
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ClientTowFormController {
+    public TextField txtFieldSendMsg;
+    public Button btnSend;
+    public TextArea textAreaDisplay;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
